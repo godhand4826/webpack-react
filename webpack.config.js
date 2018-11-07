f96fe1e490edfa682af94058b70952fc6ee0af52
Eric Huang <godhand4826@gmail.com>
2018-11-07 21:16:02 +0800

    simple components, html-loader

--- webpack.config.js
+++ webpack.config.js
@@ -1,13 +1,34 @@
+const path = require('path')
+const HtmlWebPackPlugin = require('html-webpack-plugin');
+
 module.exports = {
+    entry: {
+        index: path.resolve(__dirname, 'src/index.js')
+    },
+    output: {
+        path: path.resolve(__dirname, 'dist')
+    },
     module: {
         rules: [
             {
                 test: /\.js$/,
                 exclude: /node_modules/,
                 use: {
-                    loader: "babel-loader"
+                    loader: 'babel-loader'
+                }
+            },
+            {
+                test: /\.html$/,
+                use: {
+                    loader: 'html-loader'
                 }
             }
         ]
-    }
+    },
+    plugins: [
+        new HtmlWebPackPlugin({
+            template: path.resolve(__dirname, 'index.html'),
+            filename: 'index.html'
+        })
+    ]
 };
