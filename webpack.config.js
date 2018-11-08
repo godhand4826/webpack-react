f4f5636905e61396ebf4bf02336a592f980e1299
Eric Huang <godhand4826@gmail.com>
2018-11-08 11:37:27 +0800

    remove duplicate index.html

--- webpack.config.js
+++ webpack.config.js
@@ -27,7 +27,7 @@ module.exports = {
     },
     plugins: [
         new HtmlWebPackPlugin({
-            template: path.resolve(__dirname, 'index.html'),
+            template: path.resolve(__dirname, 'src/index.html'),
             filename: 'index.html'
         })
     ]
